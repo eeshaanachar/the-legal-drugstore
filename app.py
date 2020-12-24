80684aac28405110dc4e126f1ee676edc8a4f265
Eeshaan Achar <eeshaan77@gmail.com>
2020-12-24 13:55:45 +0530

    converted website into flask app

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import sqlite3
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    with sqlite3.connect('database.db') as connection:
+        results = connection.execute('select * from drugs').fetchall()
+    keys = 'id name short_desc long_desc link'.split()
+    products = [dict(zip(keys, result)) for result in results]
+    return render_template('index.html', products=products)
+
+@app.route('/api')
+def foo():
     response = {
-        'message' : 'backend is live!',
         'endpoints' : {
-            '/all' : 'get a list of all drugs',
+            '/all' : 'get details of all drugs',
             '/<id>' : 'get details of drug with given id',
             '/images/<id>.jpg' : 'get image of drug with given id', # Must be served by NGINX
         }
     }
     return jsonify(response)
 
-@app.route('/all')
+@app.route('/api/all')
 def all_items():
-    keys = 'id name short_desc link'.split()
     with sqlite3.connect('database.db') as connection:
-        results = connection.execute('select ' + ', '.join(keys) + ' from drugs').fetchall()
+        results = connection.execute('select * from drugs').fetchall()
+    keys = 'id name short_desc long_desc link'.split()
     response = jsonify([dict(zip(keys, result)) for result in results])
     response.status_code = 201
     return response
 
-@app.route('/<id>')
+@app.route('/api/<id>')
 def particular_item(id):
     with sqlite3.connect('database.db') as connection:
         result = connection.execute('select * from drugs where id = ?', (id, )).fetchone()
